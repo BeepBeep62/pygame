5e45d78256e6d4b621191fe0a0b2f516bc623f60
BeepBeep62 <samiaakhtar17@gmail.com>
2025-01-29 17:51:04 +0500

    tkinter doesnt work imsad

---
+++ tkemplate.py
@@ -0,0 +1,10 @@
+# Write a Python program to - create an empty Tkinter window, set the title to it, and set its geometry.
+import tkinter as tk
+from tkinter import *
+Window = Tk()
+Window.title("tk")
+Window.geometry("410x390")
+# Write a program to create a Pygame window that takes the user's name as input and displays a personalized message. Use labels, buttons, entry, and text widgets to develop this application.
+liberal = Label(text="eyes",fg="white",bg="blue")
+butler = Button(text="button click now for free 439020338089122 robux now now NOW!!!",fg="white",bg="blue")
+envy = Label(text="enter ur Legal name")
